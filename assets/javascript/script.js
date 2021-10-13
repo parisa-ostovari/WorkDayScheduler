1c4023f73667f541771cd8eadc2089a850823132
parisa-ostovari <parisaostovari@gmail.com>
2021-10-12 21:31:10 -0700

    click event and basic function for local storage began in JS

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,21 @@
 var currentDay = moment().format("dddd, MMMM Do");
     $("#currentDay").append(currentDay);
 
+$(function() {
+    //click button
+    $(".saveBtn").on("click", function () {
+
+        // get the nearby values
+        var text = $(this).siblings('.description');
+        var time = $(this).parent().attr('id');
+        
+        // save in local storage
+        localStorage.setItem(time, text);
+
+        console.log(text);
+    });
+
+
+
+    
+});
