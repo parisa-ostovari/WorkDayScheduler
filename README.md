1bbbd54474f7bda9b98369c80eb4b4446bc14272
parisa-ostovari <parisaostovari@gmail.com>
2021-10-16 08:57:03 -0700

    finished project - html/javascript updated - readme added

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # WorkDayScheduler
+
+This is a simple calendar application that allows the user to save tasks for each hour of the day and stores it into the local storage. As the day progresses the hours in the past will show up in grey, the current hour will change to red, and the past hour will turn to green.
+ 
+## Acceptance Criteria
+```
+GIVEN I am using a daily planner to create a schedule
+
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+
+WHEN I click into a timeblock
+THEN I can enter an event
+
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+
+WHEN I refresh the page
+THEN the saved events persist
+```
+
+## Link to Deployed Application
+
+hhttps://parisa-ostovari.github.io/WorkDayScheduler/
+
+## Link to GitHub Repository
+
+https://github.com/parisa-ostovari/WorkDayScheduler
+
+## Screenshot
+Here is the snapshot of how the website should look like:
+ 
+ When first opened:
+![alt="Password Generator Screenshot"](./images/SchedulerScreenshot.png)
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,21 +1,52 @@
-var currentDay = moment().format("dddd, MMMM Do");
+// display today's day and date 
+var currentDay = moment().format('dddd, MMM Do YYYY LT');
     $("#currentDay").append(currentDay);
 
-$(function() {
-    //click button
-    $(".saveBtn").on("click", function () {
 
-        // get the nearby values
-        var text = $(this).siblings('.description');
-        var time = $(this).parent().attr('id');
-        
-        // save in local storage
-        localStorage.setItem(time, text);
+$(".saveBtn").on("click", function () {
 
-        console.log(text);
-    });
-
-
-
+    // get the nearby values 
+    var text = $(this).siblings('.description').val();
+    var time = $(this).parent().attr('id');
     
+    // ave in local storage
+    localStorage.setItem(time, text);
+    console.log(text);
 });
+
+
+function hour(){
+// current number of hours
+var currentHour = moment().hours();
+
+$('.time-block').each(function(){
+
+    var rowHour = parseInt($(this).attr('id'))
+
+    // to check current hour vs. the rows hour
+    if(rowHour < currentHour){
+        $(this).addClass('past');
+    }else if(rowHour === currentHour){
+        $(this).removeClass('past');
+        $(this).addClass('present');
+    }else{
+        $(this).removeClass('past');
+        $(this).removeClass('present');
+        $(this).addClass('future');
+    }
+})
+
+}
+
+hour();
+
+$('#7 .description').val(localStorage.getItem('7'));
+$('#8 .description').val(localStorage.getItem('8'));
+$('#9 .description').val(localStorage.getItem('9'));
+$('#10 .description').val(localStorage.getItem('10'));
+$('#11 .description').val(localStorage.getItem('11'));
+$('#12 .description').val(localStorage.getItem('12'));
+$('#13 .description').val(localStorage.getItem('13'));
+$('#14 .description').val(localStorage.getItem('14'));
+$('#15 .description').val(localStorage.getItem('15'));
+$('#16 .description').val(localStorage.getItem('16')); 
